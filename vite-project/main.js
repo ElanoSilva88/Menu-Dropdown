2f8cbbfbdfb64f3c34a836a80e529c20ead86715
Elano Gomes Da Silva <elano0587@digitalcollegesul.local>
2023-12-15 11:52:11 -0300

    Implementando JS

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,17 @@
 import './style.css'
 
+function menuPal(){
+    const btnPal = document.querySelector('.timeTimes');
+    const timePal = document.querySelector('.listaJogadoresPal');
+    btnPal.addEventListener('click', () => {
+        console.log('quadfsf')
+        if (timePal.classList.contains('hidden')) {
+            timePal.classList.remove('hidden')
+        } else {
+            timePal.classList.add('hidden')
+        }
+
+    })
+}
+
+menuPal()
